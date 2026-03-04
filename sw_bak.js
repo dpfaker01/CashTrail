7da27543602d104936ccd7d0e31e1397f7ec1bfb
dpfaker01 <112249907+dpfaker01@users.noreply.github.com>
2026-03-04 21:42:57 +0800

    Update sw_bak.js

--- sw_bak.js
+++ sw_bak.js
@@ -1,23 +1,23 @@
-const CACHE_NAME = 'cashtrail-v1';
+const CACHE_NAME = 'cashtrail-v3-fifo';
 const BASE_PATH = '/CashTrail';
 
 const STATIC_ASSETS = [
   `${BASE_PATH}/`,
   `${BASE_PATH}/index.html`,
   `${BASE_PATH}/manifest.json`,
-  `${BASE_PATH}/icon-72x72.png`,
-  `${BASE_PATH}/icon-96x96.png`,
-  `${BASE_PATH}/icon-128x128.png`,
-  `${BASE_PATH}/icon-144x144.png`,
-  `${BASE_PATH}/icon-152x152.png`,
-  `${BASE_PATH}/icon-192x192.png`,
-  `${BASE_PATH}/icon-384x384.png`,
-  `${BASE_PATH}/icon-512x512.png`
+  `${BASE_PATH}/icons/icon-72x72.png`,
+  `${BASE_PATH}/icons/icon-96x96.png`,
+  `${BASE_PATH}/icons/icon-128x128.png`,
+  `${BASE_PATH}/icons/icon-144x144.png`,
+  `${BASE_PATH}/icons/icon-152x152.png`,
+  `${BASE_PATH}/icons/icon-192x192.png`,
+  `${BASE_PATH}/icons/icon-384x384.png`,
+  `${BASE_PATH}/icons/icon-512x512.png`
 ];
 
 // Install event - cache static assets
 self.addEventListener('install', (event) => {
-  console.log('CashTrail: Service Worker installing');
+  console.log('CashTrail v3.0: Service Worker installing');
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
@@ -34,7 +34,7 @@ self.addEventListener('install', (event) => {
 
 // Activate event - clean up old caches
 self.addEventListener('activate', (event) => {
-  console.log('CashTrail: Service Worker activating');
+  console.log('CashTrail v3.0: Service Worker activating');
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
@@ -53,7 +53,7 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   // Skip non-GET requests
   if (event.request.method !== 'GET') return;
-  
+
   // Skip cross-origin requests
   const url = new URL(event.request.url);
   if (url.origin !== self.location.origin) return;
@@ -88,3 +88,11 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
+// Message event - handle skip waiting
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
